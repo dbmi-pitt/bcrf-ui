ee0278b2d86fbf90f89958a919149d7c3cb55ff9
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-15 10:51:41 -0400

    Update fetch

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -3,6 +3,11 @@ import log from 'xac-loglevel';
 const URLS = {
    api: {
     local: (path) => `/api/${path}`,
+   },
+   content: {
+      banner: process.env.NEXT_PUBLIC_CONTENT_BANNER_URL ||
+    `${process.env.NEXT_PUBLIC_APP_BASE_URL}content/banner.json`,
+      summary: `${process.env.NEXT_PUBLIC_APP_BASE_URL}content/summary-data-sources.json`
    }
 };
 
