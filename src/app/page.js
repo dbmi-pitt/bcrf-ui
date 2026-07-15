c4f013722c5f815faef588f83b76e8fbfeab649e
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-15 14:28:55 -0400

    Add filtering

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffectEvent, useEffect} from 'react';
 import BasicLayout from '@/components/layout/BasicLayout';
 import AppSpinner from '@/components/AppSpinner';
 import AppContext from '@/context/AppContext';
@@ -10,23 +10,68 @@ import { Container, Row } from 'react-bootstrap';
 export default function Home() {
   const { content } = useContext(AppContext)
   const [tags, setTags] = useState([])
+  const [cards, setCards] =  useState(content?.summary?.data_sources || [])
+
+  const setDataSources = useEffectEvent(() => {
+    setCards(content?.summary?.data_sources)
+  }, [])
+
+  useEffect(() => {
+    if (content.summary) {
+      setDataSources()
+    }
+  }, [content.summary])
+
+  const filterCards = (tag, value, sources = []) => {
+    const sourceIds = sources.map((d) => d.source)
+    const dict = {}
+    sourceIds.forEach(role => {
+      dict[role] = true;
+    });
+    // filter out the cards already included
+    const availableSources = content?.summary?.data_sources.filter((d) => dict[d.source] === undefined)
+    availableSources.map((data) => {
+      data.tags.map((t) => {
+        if (dict[data.source] === undefined && (t.name === tag.name && t.values.indexOf(value) !== -1)) {
+          sources.push(data)
+          dict[data.source] = true
+        }
+      })
+    })
+    return sources
+  }
 
   const onCardTagClick = ({data, tag, value}) => {
-    setTags([...tags, {name: tag.name, value}])
+    const sources = filterCards(tag, value)
+    setCards(sources)
+    setTags([...tags, {name: tag.name, value, id: data.source}])
   }
 
   const getHeaderTags = () => {
-    const list = []
+    const list = [];
     for (const t of tags) {
-      list.push(<Tag className='c-tag' key={`${t.name}-${t.value}`} closable onClick={() => onHeaderTagClick(t)} >
-                      <strong>{t.name}</strong>: {t.value}
-                </Tag>)
+      list.push(
+        <Tag
+          className="c-tag"
+          key={`${t.name}-${t.value}`}
+          closable
+          onClose={() => onHeaderTagClick(t)}
+        >
+          <strong>{t.name}</strong>: {t.value}
+        </Tag>,
+      );
     }
     return list;
-  }
+  };
 
   const onHeaderTagClick = (tag) => {
-    setTags(tags.filter((t) => (t.name !== tag.name && t.value !== tag.value)))
+    const newTags = tags.filter((t) => (t.id === tag.id && t.value !== tag.value))
+    let sources = []
+    for (const t of newTags) {
+      sources = filterCards(t, t.value, sources)
+    }
+    setCards(sources.length ? sources : content?.summary?.data_sources)
+    setTags(newTags)
   }
 
   return (
@@ -35,10 +80,10 @@ export default function Home() {
         <Row className='c-selectedTags'>
           <Container>{getHeaderTags()}</Container>
         </Row>
-        {content.summary && <Masonry
+        {cards && <Masonry
           columns={{xs: 1, sm: 2, md: 3}}
           gutter={10}
-          items={content?.summary?.data_sources.map((source, index) => ({
+          items={cards.map((source, index) => ({
             key: `item-${index}`,
             data: source,
           }))}
