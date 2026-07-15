9475f2279b36c75ec23ba6d195ef736261fc8cd0
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-15 11:06:51 -0400

    Pull from data sources

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
 'use client';
+import React, {useContext} from 'react';
 import BasicLayout from '@/components/layout/BasicLayout';
 import AppSpinner from '@/components/AppSpinner';
+import AppContext from '@/context/AppContext';
+import { Masonry } from 'antd';
+import AppCard from '@/components/AppCard';
 
 export default function Home() {
-
+  const {content} = useContext(AppContext)
 
   return (
     <div>
       <BasicLayout fluid={undefined}>
+        {content.summary && <Masonry
+          columns={4}
+          gutter={16}
+          items={content.summary.data_sources}
+          itemRender={({ data, index }) => (
+            <AppCard data={data} key={`card-${index}`} />
+          )}
+        />}
       </BasicLayout>
     </div>
   );
