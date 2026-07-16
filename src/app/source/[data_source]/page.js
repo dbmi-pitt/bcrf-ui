00af7116614c71043c297745075899fb88c999b8
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-16 09:30:03 -0400

    Remove snake_case from react folder

--- src/app/source/[data_source]/page.js
+++ src/app/source/[data_source]/page.js
@@ -1,14 +1,11 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import GridLayout from '@/components/grid_layout/GridLayout';
+import GridLayout from '@/components/grid/GridLayout';
 
 export default function Page() {
   const params = useParams();
   const data_source = params.data_source;
 
-  return(
-    <GridLayout/>
-  )
-
+  return <GridLayout />;
 }
