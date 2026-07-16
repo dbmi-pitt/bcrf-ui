1788b52920a3acd1be357b457624d0e499b852c7
Tyler Madonna <tjm159@pitt.edu>
2026-07-16 15:33:55 -0400

    Add filtering support for data

--- src/lib/data/index.js
+++ src/lib/data/index.js
@@ -1,5 +1,7 @@
 'use server';
 
+import { buildFilterClause } from './filter.js';
+
 const sourceMap = {
   'aurora-us': (await import('./config/auroraUS.js')).CONFIG,
   'aurora-eu': (await import('./config/auroraEU.js')).CONFIG,
@@ -15,4 +17,53 @@ export const getChartData = async (sourceId) => {
   return {
     charts: config.charts.map(({ filterColumn, query, ...rest }) => rest),
   };
+};
+
+const exampleInputFilters = {
+  'cancer-type-detailed': ['Breast Invasive Ductal Carcinoma'],
+  'pathologic-stage': ['Stage IIA'],
+};
+
+const exampleMappedFilters = {
+  'Cancer Type Detailed': ['Breast Invasive Ductal Carcinoma'],
+  'Pathologic Stage': ['Stage IIA'],
+};
+
+export const getFilteredChartData = async (sourceId, filters) => {
+  const config = sourceMap[sourceId];
+  if (!config) {
+    return { notFound: true };
+  }
+
+  const mappedFilters = {};
+  for (const [key, value] of Object.entries(filters)) {
+    // skip empty filters
+    if (!value || !value.length) {
+      continue;
+    }
+
+    // find the chart config for this filter key
+    const chart = config.charts.find((c) => c.filterColumn === key);
+    if (!chart) {
+      continue;
+    }
+
+    // skip if the chart if not filterable
+    const column = chart.filterColumn;
+    if (!column) {
+      continue;
+    }
+
+    mappedFilters[column] = value;
+  }
+
+  // if no valid mapped filters, return the chart data without filtering
+  if (!mappedFilters || !Object.keys(mappedFilters).length) {
+    return {
+      data: config.charts.map(({ id, data }) => ({ id, data })),
+      filters: {},
+    };
+  }
+
+  const { clause, params } = buildFilterClause(mappedFilters);
 };
