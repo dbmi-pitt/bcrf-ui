2441376c614d4e75dbfd67f800a166a9c77ae157
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-22 16:00:07 -0400

    Adjust chart bars

--- src/components/charts/Histogram.jsx
+++ src/components/charts/Histogram.jsx
@@ -44,6 +44,7 @@ function sortIntoBins(bins, data) {
 
 const HistogramMinBar = (props) => {
   if (props.index === props.rawData.bins.length - 2) return <></>
+  const {rawData, chartPaddings, index} = props
 
   const minHeight = 3;
   const barProps = {...props}
@@ -51,10 +52,11 @@ const HistogramMinBar = (props) => {
   const value = props.datum.y;
   const actualHeight = Math.abs(props.y0 - props.y);
   log.info(`value: ${value} actual: ${actualHeight}`);
+  const margins = chartPaddings.left + chartPaddings.right
 
-  if (props.index !== 0 && (props.index !== props.rawData.bins.length - 1)) {
-    barProps.alignment = "start"
-    barProps.x += barProps.x0/7
+  if (index !== 0 && (index !== rawData.bins.length - 1)) {
+    barProps.alignment = "start";
+    barProps.x += ((props.width - margins) / rawData.bins.length / 4)
   }
 
   // Don't modify zero-value bars
@@ -86,48 +88,56 @@ function Histogram({ data, width, height }) {
     count: count,
   }));
 
+  const chartPaddings = {
+    left: 50,
+    right: 20,
+    top: 10,
+    bottom: 60,
+  };
+
   return (
     <div className="c-chart__histogram">
       <VictoryChart
         domainPadding={{ x: 50, y: 10 }}
-        padding={{
-                            left: 50,
-                            right: 20,
-                            top: 10,
-                            bottom: 60,
-                        }}
+        padding={chartPaddings}
         width={width}
         height={height}
         theme={VictoryTheme.clean}
       >
         <VictoryBar
           data={histogramData}
-          dataComponent={<HistogramMinBar rawData={data} />}
+          dataComponent={<HistogramMinBar rawData={data} chartPaddings={chartPaddings} />}
           x="bin"
           y="count"
-           labels={(props) => {
-            const {datum, data} = props
-            const index = props.index
-            let inclusiveLabel = data[index + 1]?.bin
+          labels={(props) => {
+            const { datum, data } = props;
+            const index = props.index;
+            let inclusiveLabel = data[index + 1]?.bin;
             if (index === data.length - 2) {
-              const bin = binnedData.filter((b) => b.label === inclusiveLabel)
-              inclusiveLabel = bin[0].bin.value
+              const bin = binnedData.filter((b) => b.label === inclusiveLabel);
+              inclusiveLabel = bin[0].bin.value;
             }
             const range =
               index === 0 || index === data.length - 1
                 ? datum.bin
                 : `(${data[index].bin}, ${inclusiveLabel}]`;
-            return `Number of samples: ${datum.count}\nRange: ${range}`
+            return `Number of samples: ${datum.count}\nRange: ${range}`;
           }}
-          labelComponent={<VictoryTooltip constrainToVisibleArea dx={(props) => {
-            if (props.index === 0 || props.index === binnedData.length - 1) return 0;
-            return 15
-          }} />}
+          labelComponent={
+            <VictoryTooltip
+              constrainToVisibleArea
+              dx={(props) => {
+                if (props.index === 0 || props.index === binnedData.length - 1)
+                  return 0;
+                return 15;
+              }}
+            />
+          }
         />
         <VictoryAxis label={data.labels.y} dependentAxis />
         <VictoryAxis
           label={data.labels.x}
-          
+
           style={THEME.chart.ticks.style}
         />
       </VictoryChart>
