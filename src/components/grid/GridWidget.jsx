9df28dd80c223a440d943f12cd6b67c8c2233f3b
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-23 11:55:07 -0400

    Add popover

--- src/components/grid/GridWidget.jsx
+++ src/components/grid/GridWidget.jsx
@@ -10,9 +10,10 @@ import {
   TableOutlined,
 } from '@ant-design/icons';
 import { Dropdown, Tooltip } from 'antd';
-import { useState } from 'react';
+import { useState,  useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import WidgetPopover from '../WidgetPopover';
 
 export default function GridWidget({
   title,
@@ -28,6 +29,8 @@ export default function GridWidget({
   setLegend
 }) {
   const [chartType, setChartType] = useState(chart.types[0]);
+  const [widgetPopover, setShowWidgetPopover] = useState(null)
+  const widgetRef = useRef(null)
 
   const data = { ...chart };
 
@@ -170,6 +173,9 @@ export default function GridWidget({
         className="d-flex flex-column c-gridWidget__main"
         style={{ height: 0, flex: 1, padding: 1 }}
         id={widgetBodyId}
+        ref={widgetRef}
+        onMouseOver={(e) => setShowWidgetPopover(e)}
+        onMouseOut={(e) => setShowWidgetPopover(null)}
       >
         <ChartProvider 
             isFilterable={isFilterable}
@@ -179,11 +185,13 @@ export default function GridWidget({
             legend={legend} 
             setLegend={setLegend}
             >
-          {/* // TODO: build over over legend table for pie chart */}
+     
           <Chart
             data={data}
             chartType={chartType}
           />
+          {widgetPopover && ['table', 'scatter'].indexOf(chartType) == -1 && <WidgetPopover event={widgetPopover} data={data} targetRef={widgetRef}
+            chartType={chartType}  />}
         </ChartProvider>
       </Card.Body>
     </Card>
