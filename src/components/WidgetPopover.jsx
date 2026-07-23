ca074ee96cf28f985ac14fa247f4de9228985674
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-23 11:55:56 -0400

    Update title

--- src/components/WidgetPopover.jsx
+++ src/components/WidgetPopover.jsx
@@ -8,7 +8,7 @@ const WidgetPopover = ({event, data, chartType}) => (
   <div className='c-widgetPopover'>
     <OverlayTrigger target={event.target} show={true} placement="auto" overlay={
     <Popover className='c-widgetPopover__main'>
-    <Popover.Header as="h3">{data.id}</Popover.Header>
+    <Popover.Header as="h3">{data.title}</Popover.Header>
     <Popover.Body>
       <Tabula data={data} />
       {/* TODO Other charts */}
