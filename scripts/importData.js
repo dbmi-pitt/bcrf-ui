c80518c89ae906bde9678bdd3f2470b6af504060
Tyler Madonna <tjm159@pitt.edu>
2026-07-29 11:54:50 -0400

    Replace table in import data

--- scripts/importData.js
+++ scripts/importData.js
@@ -12,7 +12,7 @@ const dataDir = path.resolve(DATA_DIR);
 
 // aurora us
 await connection.run(`
-    CREATE TABLE aurora_us AS
+    CREATE OR REPLACE TABLE aurora_us AS
     SELECT
         * REPLACE (
             TRY_CAST(TRIM("Age at Diagnosis") AS DOUBLE)                 AS "Age at Diagnosis",
@@ -30,7 +30,7 @@ await connection.run(`
 
 // aurora eu
 await connection.run(`
-    CREATE TABLE aurora_eu AS
+    CREATE OR REPLACE TABLE aurora_eu AS
     SELECT
         * REPLACE (
             TRY_CAST(TRIM("overall_survival_days") AS INTEGER)           AS "overall_survival_days",
