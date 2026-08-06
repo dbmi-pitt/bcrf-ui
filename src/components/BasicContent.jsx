4155c5002c0d5c10780be41a2a03590092f39859
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-08-06 14:55:04 -0400

    Update classes

--- src/components/BasicContent.jsx
+++ src/components/BasicContent.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Container, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import Image from "next/image";
 
 function BasicContent({content}) {
   return (
-    <Container className="c-basicContent">
+    <div className="c-basicContent">
       <Row className="mb-5">
         {content.image && <Image src={content.image} alt={content.title} width="190" height="119" className="c-box--contentPage__image w-fixed" />}
-        {content.title && <h1 className="fs-1">{content.title}</h1>}
+        {content.title && <h2 className="p4 mt-4">{content.title}</h2>}
         <div className="c-box--contentPage__body" dangerouslySetInnerHTML={{ __html: content.body }} />
       </Row>
-      </Container>
+      </div>
   )
 }
 
