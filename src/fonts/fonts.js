b265f578ffb84de310f435f1973929762e8b68bb
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-08-06 11:03:03 -0400

    Gather footer and header

---
+++ src/fonts/fonts.js
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const hurmeRegular = localFont({
+  src: "./Hurme-Geometric-Sans-4/HurmeGeometricSans4.otf",
+});
+
+export const hurmeThin = localFont({
+  src: "./Hurme-Geometric-Sans-4/HurmeGeometricSans4 Thin.otf",
+});
+
+export const hurmeSemiBold = localFont({
+  src: "./Hurme-Geometric-Sans-4/HurmeGeometricSans4 SemiBold.otf",
+});
