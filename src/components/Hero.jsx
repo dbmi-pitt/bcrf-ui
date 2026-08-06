b6cf1280f7b2519165a4cb0fc0ea523b3bf732c5
maxsibilla <maxsibilla@icloud.com>
2026-08-06 16:28:30 -0400

    Adding modal to home page and adjusting link to /sources

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
-"use client";
+'use client';
 
-import React, { useContext } from "react";
-import { Col, Row } from "react-bootstrap";
-import LinkButton from "@/components/LinkButton";
+import React, { useContext, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import LinkButton from '@/components/LinkButton';
 import AppContext from '@/context/AppContext';
+import Button from 'react-bootstrap/Button';
 
 const Hero = ({ content }) => {
   const { isAuthenticated } = useContext(AppContext);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   return (
     <section
@@ -21,27 +27,45 @@ const Hero = ({ content }) => {
         <div className="c-hero__row row c-hero__body fs-1 lh-sm">
           <p>{content.body}</p>
         </div>
-        {content.btns.length > 0 && <Row xs={1} md={2} className="c-hero__footer">
-          <Col>
-            {!isAuthenticated && content.btns[0] && (
-              <LinkButton
-                className=" text-white mt-4"
-                href={content.btns[0].href}
-              >
-                {content.btns[0].text}
-              </LinkButton>
-            )}
+        {content.btns.length > 0 && (
+          <Row xs={1} md={2} className="c-hero__footer">
+            <Col>
+              {!isAuthenticated && content.btns[0] && (
+                <>
+                  <LinkButton className=" text-white mt-4" onClick={handleShow}>
+                    {content.btns[0].text}
+                  </LinkButton>
 
-            {isAuthenticated && content.btns[1] && (
-              <LinkButton
-                className="text-white mt-4"
-                href={content.btns[1].href}
-              >
-                {content.btns[1].text}
-              </LinkButton>
-            )}
-          </Col>
-        </Row>}
+                  <Modal show={showModal} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Sign Up!</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      Thank you for your interest!<br></br>
+                      <br></br>
+                      Please contact us via
+                      <a href="mailto:BCRFGDH@pitt.edu">BCRFGDH@pitt.edu</a>.
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
+              )}
+
+              {isAuthenticated && content.btns[1] && (
+                <LinkButton
+                  className="text-white mt-4"
+                  href={content.btns[1].href}
+                >
+                  {content.btns[1].text}
+                </LinkButton>
+              )}
+            </Col>
+          </Row>
+        )}
       </div>
     </section>
   );
