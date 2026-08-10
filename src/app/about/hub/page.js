ea0e83b72db9a110c91ab13367e57df0bf630b5a
Tyler Madonna <tjm159@pitt.edu>
2026-08-10 09:34:58 -0400

    Add titles to about pages

--- src/app/about/hub/page.js
+++ src/app/about/hub/page.js
@@ -1,5 +1,7 @@
 import AppPage from '@/components/AppPage';
 
+export const metadata = { title: 'About Data Hub' };
+
 export default function Page() {
   return <AppPage path="/about/hub" />;
 }
--- src/app/about/partnership/page.js
+++ src/app/about/partnership/page.js
@@ -1,5 +1,7 @@
 import AppPage from '@/components/AppPage';
 
+export const metadata = { title: 'About Partnership' };
+
 export default function Page() {
   return <AppPage path="/about/partnership" />;
 }
