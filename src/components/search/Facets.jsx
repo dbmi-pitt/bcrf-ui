8bd62f7a2ade88c1aac56e41f4fd97033adcd5a8
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-09-02 14:55:22 -0400

    Prepare for real data

--- src/components/search/Facets.jsx
+++ src/components/search/Facets.jsx
@@ -2,65 +2,65 @@ import React, { useContext, useEffect, useMemo, useState } from 'react';
 import SearchContext from '@/context/SearchContext';
 import { Tree, ConfigProvider } from 'antd';
 import log from 'xac-loglevel';
+import { DEMO } from '@/lib/demo';
 
 function Facets({}) {
   const { config, facets, setFacets } = useContext(SearchContext);
-
-  const getFilterGroup = (groupIds) => {
-    let andFilter = {length: groupIds[0].length, index: 0};
-    for (let i = 1; i < groupIds.length; i++) {
-      if (groupIds[i].length > 0) {
-        andFilter = {
-          length: Math.min(andFilter.length, groupIds[i].length) || andFilter.length,
-          index: groupIds[i].length < andFilter.length || andFilter.length === 0 ? i : andFilter.index,
-        };
-        andFilter.length = groupIds[andFilter.index].length;
-      }
-    }
-    return andFilter;
-
-  }
-  const filterSources = (facetName) => {
-    const groups = config.facets?.map((facet) => facet.sources) || {};
-    const groupIds = [];
-    let sourceIds;
-
-    for (const sources of groups) {
-      sourceIds = new Set();
-      for (const source in sources) {
-        const facets = sources[source].value_map || {};
-        for (const facet in facets) {
-          if (facets[facet] === facetName) {
-            sourceIds.add(source);
-          }
-        }
-      }
-      groupIds.push(Array.from(sourceIds));
-    }
-
-    if (groupIds.length === 0) {
-      return [];
-    }
-
-    let andFilter = getFilterGroup(groupIds);
-
-    const filteredSources = config.dataSources.filter((source) =>
-      groupIds[andFilter.index].includes(source.source),
-    );
-    return filteredSources;
-  };
+  const [aggregatedFacets, setAggregatedFacets] = useState([]);
 
   const onCheck = (checkedKeys, info) => {
+    log.debug('Facets: onCheck', checkedKeys, info);
+    setFacets(checkedKeys);
+    config.setIsBusy(true);
+
+    
+    const checkedFacets = info.checkedNodes
+      .filter((node) => node.children === undefined)
+    
+    const filters = {}
+    for (const facet of checkedFacets) {
+      const parent = facet.parent;
+
+      if (!filters[parent]) {
+        filters[parent] = new Set();
+      }
+      filters[parent].add(facet.field);
+    }
+
+    // TODO: get the filtered sources based on the filters object
+    const result = getFilteredSources(filters);
+    if (result.success) {
+      config.setCards(result.sources);
+
+      const aggs = {}
+      for (const agg in result.aggregations) {
+        if (!aggs[agg]) {
+          aggs[agg] = {}
+        }
+        for (const value of result.aggregations[agg]) {
+          aggs[agg][value.term] = value.count
+        }
+      }
+      setAggregatedFacets(aggs);
+    }
+    
+    config.setIsBusy(false);
+  }
+
+  const onCheckDemo = (checkedKeys, info) => {
+    // This is a demo implementation of filtering logic based on checked facets
+    log.debug('Facets: onCheckDemo', checkedKeys, info, treeData);
     setFacets(checkedKeys);
     config.setIsBusy(true);
 
     const filteredSources = [];
+
     const checkedFacets = info.checkedNodes
       .filter((node) => node.children === undefined)
-      .map((node) => node.title);
+      .map((node) => node.field);
 
     for (const facet of checkedFacets) {
-      filteredSources.push(filterSources(facet));
+      filteredSources.push(DEMO.filterSources(config, facet));
     }
 
     if (filteredSources.length === 0) {
@@ -71,14 +71,14 @@ function Facets({}) {
       return;
     }
 
-    const andFilter = getFilterGroup(filteredSources);
+    const andFilter = DEMO.getFilterGroup(filteredSources);
     let filteredSourcesArray = filteredSources[andFilter.index] || [];
     filteredSourcesArray = Array.from(new Set(filteredSourcesArray));
     config.setCards(
       info.checkedNodes.length ? filteredSourcesArray : config.dataSources,
     );
     config.setIsBusy(false);
-    log.debug('Facets: onCheck', checkedKeys, info);
+    
   };
 
   const defaultExpandedKeys = useMemo(() => {
@@ -108,22 +108,30 @@ function Facets({}) {
     return checkedKeys;
   }, [config]);
 
-  const treeData = useMemo(() => {
+
+  const getTreeData = () => {
     if (!config) return [];
     const facets = config.facets || [];
     return facets.map((facet, index) => {
       return {
         title: facet.name,
+        field: facet.name,
         key: `0-${index}`,
         children: facet.values.map((value, valueIndex) => {
           return {
-            title: value,
+            title: aggregatedFacets[facet.name]?.[value] !== undefined
+              ? `${value} (${aggregatedFacets[facet.name][value]})`
+              : value,
+            field: value,
             key: `0-${index}-${valueIndex}`,
+            parent: facet.name,
           };
         }),
       };
     });
-  }, [config]);
+  }
+
+  const treeData = useMemo(() => getTreeData(), [config]);
 
 
 
@@ -144,7 +152,7 @@ function Facets({}) {
         defaultExpandedKeys={defaultExpandedKeys}
         defaultCheckedKeys={defaultCheckedKeys}
         checkedKeys={facets}
-        onCheck={onCheck}
+        onCheck={onCheckDemo}
         treeData={treeData}
       />
       
