de8ea43dfdf93f07a606fbf1ca8002bcbb2c9539
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-09-09 10:32:24 -0400

    Add legend

--- src/components/charts/GroupedColumn.jsx
+++ src/components/charts/GroupedColumn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Bar,
+  VictoryLegend,
   VictoryAxis,
   VictoryGroup,
   VictoryBar,
@@ -17,8 +17,8 @@ const getBars = (data) => {
     bars.push(
       <VictoryBar
         animate={{
-          duration: 1000,      
-          onLoad: { duration: 500 } 
+          duration: 1000,
+          onLoad: { duration: 500 },
         }}
         style={
           THEME.colors[group]
@@ -36,17 +36,26 @@ const getBars = (data) => {
   return bars;
 };
 
+const getLegendData = (data) => {
+  const legend = [];
+  for (const name in data) {
+    legend.push({
+      name,
+      symbol: { fill: THEME.colors[name] },
+    });
+  }
+  return legend;
+};
+
 function GroupedColumn({ data, width, height }) {
-  
   return (
-    <div className="c-chart__groupedColumn" style={{ width: '100%', maxWidth: '2000px' }}>
-      <VictoryChart 
-        theme={VictoryTheme.clean}>
-        <VictoryGroup
-          offset={50}
-          >
-          {getBars(data)}
-        </VictoryGroup>
+    <div
+      className="c-chart__groupedColumn"
+      style={{ width: '100%', maxWidth: '2000px' }}
+    >
+      <VictoryChart theme={VictoryTheme.clean}>
+        <VictoryGroup offset={50}>{getBars(data)}</VictoryGroup>
+        <VictoryLegend x={125} y={20} data={getLegendData(data)} />
       </VictoryChart>
     </div>
   );
