1889f09309975007c84bceb1af1203ac74cd882f
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-09-10 10:58:49 -0400

    Adjust visual - #138

--- src/components/charts/GroupedColumn.jsx
+++ src/components/charts/GroupedColumn.jsx
@@ -13,42 +13,33 @@ import {
 import THEME from '@/lib/theme';
 
 const DoubleLabel = (props) => {
-  const { x, y, text, datum, scale } = props;
-
+  const { x, y, datum, scale } = props;
   const pixelY = scale.y(0);
-
   return (
     <g>
       <text
         x={x}
-        y={y - 10} // Offset vertically above the data point
+        y={y - 10}
         textAnchor="middle"
-        fill={'#333'} 
+        fill={'#333'}
         style={{ fontSize: 12, fontWeight: 'bold' }}
       >
         {datum.value}
       </text>
       <text
         x={x}
-        y={pixelY + 15} 
+        y={pixelY + 15}
         textAnchor="middle"
-        fill={'#333'} 
+        fill={'#333'}
         style={{ fontSize: 12, fontWeight: 'bold' }}
       >
         {datum.total}
       </text>
     </g>
   );
-
-  // return (<VictoryLabel
-  //           // Dynamic vertical offset based on positive or negative values
-  //           dy={({ datum }) => (datum.y >= 0 ? -10 : 15)}
-  //           textAnchor="middle"
-  //           style={{ fill: "#333", fontSize: 12, fontWeight: "bold" }}
-  //         />)
 };
 
-const getBars = (data) => {
+const getBars = (data, style) => {
   const bars = [];
   for (const group in data) {
     bars.push(
@@ -66,8 +57,7 @@ const getBars = (data) => {
         key={group}
         data={data[group]}
         labels={({ datum }) => `${datum.y}`}
-        labelComponent={<DoubleLabel />}
-        // labelComponent={<VictoryTooltip />}
+        labelComponent={style.labelComponent || <VictoryTooltip />}
       />,
     );
   }
@@ -76,43 +66,37 @@ const getBars = (data) => {
 
 const getLegendData = (data) => {
   const legend = [];
-  const xAxis = {};
-  const groups = {};
   for (const name in data) {
-    groups[name] = 0;
-    data[name].map((datum) => {
-      groups[name] += datum.y;
-      if (!xAxis[datum.x]) {
-        xAxis[datum.x] = 0;
-      }
-      xAxis[datum.x] += datum.y;
-    });
-
     legend.push({
-      name: `${name} (${groups[name]})`, // groups totals
+      name,
       symbol: { type: 'square', fill: THEME.colors[name] },
-    });
-  }
-
-  // xaxis or source totals
-  for (const label in xAxis) {
-    legend.push({
-      name: `${label} (${xAxis[label]})`,
-      symbol: { type: 'minus', fill: THEME.colors[name] },
     });
   }
   return legend;
 };
 
-function GroupedColumn({ data, width, height }) {
+function GroupedColumn({
+  data,
+  style = {
+    minDomain: { y: 0 },
+    maxDomain: { y: 100 },
+    tickFormat: (t) => `${t}%`,
+    labelComponent: <DoubleLabel />,
+  },
+}) {
   return (
     <div className="c-chart__groupedColumn d-flex justify-content-start">
-      <VictoryChart key={JSON.stringify(data)} theme={VictoryTheme.clean} minDomain={{ y: 0 }} maxDomain={{ y: 100 }}>
-        <VictoryGroup offset={50}>{getBars(data)}</VictoryGroup>
-        <VictoryAxis dependentAxis tickFormat={(t) => `${t}%`} />
+      <VictoryChart
+        key={JSON.stringify(data)}
+        theme={VictoryTheme.clean}
+        minDomain={style.minDomain}
+        maxDomain={style.maxDomain}
+      >
+        <VictoryGroup offset={50}>{getBars(data, style)}</VictoryGroup>
+        <VictoryAxis dependentAxis tickFormat={style.tickFormat} />
         <VictoryAxis tickLabelComponent={<VictoryLabel dy={10} />} />
       </VictoryChart>
-      <VictoryLegend x={0} y={20} data={getLegendData(data)} />
+      <VictoryLegend x={0} y={100} data={getLegendData(data)} />
     </div>
   );
 }
