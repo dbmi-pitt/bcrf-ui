ea14e3d94d50a9cc7dce59a1696c9c67cb768d52
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-09-15 10:36:49 -0400

    Add dropdown menu - #106

--- src/components/layout/AppNavBar.jsx
+++ src/components/layout/AppNavBar.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import LogInIcon from '@/components/icons/LogInIcon';
-import LogOutIcon from '@/components/icons/LogOutIcon';
+import UserIcon from '@/components/icons/UserIcon';
 import RegisterIcon from '@/components/icons/RegisterIcon';
 import AuthContext from '@/context/AuthContext';
 import Image from 'next/image';
@@ -10,9 +10,10 @@ import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function AppNavBar() {
-  const { isAuthenticated, logOut } = useContext(AuthContext);
+  const { isAuthenticated, logOut, user } = useContext(AuthContext);
 
   return (
     <Navbar sticky={'top'} variant={'light'} expand="lg" className="c-navbar">
@@ -47,7 +48,7 @@ function AppNavBar() {
               {/*  </Link>*/}
               {/*</Nav>*/}
               <Nav className={'me-0'}>
-                <button
+                {/* <button
                   type="button"
                   className="c-navbar__link-button"
                   onClick={async () => {
@@ -55,8 +56,23 @@ function AppNavBar() {
                   }}
                 >
                   <span className="me-1">LOG OUT</span>
-                  <LogOutIcon className="align-baseline" />
-                </button>
+                  <UserIcon className="align-baseline" />
+                </button> */}
+                <Dropdown className='c-navbar__user'>
+                  <Dropdown.Toggle className="c-navbar__link-button" id="user-menu">
+                    <span className="me-1 text-truncate">{user.email}
+                    </span>
+                    <UserIcon className="align-baseline" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+     
+                    <Dropdown.Item href="/users">User Directory</Dropdown.Item>
+                    <Dropdown.Item  onClick={async () => {
+                    await logOut();
+                  }}>Log out</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </Nav>
             </>
           )}
