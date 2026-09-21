f87157a1bd5666f94bde5e7868091717b0b5a930
maxsibilla <maxsibilla@icloud.com>
2026-09-21 09:41:51 -0400

    Adding configuration for new source

--- src/lib/sources/charts.js
+++ src/lib/sources/charts.js
@@ -3,4 +3,5 @@ import 'server-only';
 export const sourceMap = {
   'aurora-us': (await import('@/lib/sources/config/auroraUS.js')).CONFIG,
   'aurora-eu': (await import('@/lib/sources/config/auroraEU.js')).CONFIG,
+  'upmc-ehr-bc-data-mart': (await import('@/lib/sources/config/upmcEHRDataMart')).CONFIG,
 };
